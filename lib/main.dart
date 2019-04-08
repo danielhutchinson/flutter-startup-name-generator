772e383dee5ce93180c133da4c57f7eb39f352a4
Dan Hutchinson <danielhutchinson5@gmail.com>
2019-04-08 22:11:55 +0100

    feat: now able to favorite words
    from https://codelabs.developers.google.com/codelabs/first-flutter-app-pt2/#4

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
 
 class RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
+  final _saved = new Set<WordPair>();
   final _largeFont = const TextStyle(fontSize: 18.0);
 
   Widget build(BuildContext context) {
@@ -38,11 +39,27 @@ class RandomWordsState extends State<RandomWords> {
   }
 
   Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
+
     return ListTile(
       title: Text(
         pair.asPascalCase,
         style: _largeFont,
       ),
+      trailing: new Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+          if(alreadySaved) {
+            _saved.remove(pair);
+          }
+          else {
+            _saved.add(pair);
+          }
+        });
+      },
     );
   }
 }
